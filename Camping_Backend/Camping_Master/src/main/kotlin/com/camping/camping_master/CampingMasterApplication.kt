ce544f64fc1ef8086c0ae7a61acc552a47909cf2
hanspung <hanspung@gmail.com>
2023-08-03 17:21:24 +0900

    init

---
+++ Camping_Backend/Camping_Master/src/main/kotlin/com/camping/camping_master/CampingMasterApplication.kt
@@ -0,0 +1,11 @@
+package com.camping.camping_master
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CampingMasterApplication
+
+fun main(args: Array<String>) {
+	runApplication<CampingMasterApplication>(*args)
+}
