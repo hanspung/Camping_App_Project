cf77cfbfcb3ea037614f6f14ef837031a47bf4a9
hanspung <hanspung@gmail.com>
2023-08-03 17:45:23 +0900

    테스트 커밋

---
+++ Camping_Frontend/app/src/main/java/com/example/camping_master/test/TestActivity.kt
@@ -0,0 +1,12 @@
+package com.example.camping_master.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.camping_master.R
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
